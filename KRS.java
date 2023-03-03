758a7fda47a46cdcbb4367804b947270c528b233
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2023-03-03 22:04:35 +0700

    Update KRS.java

--- KRS.java
+++ KRS.java
@@ -1,17 +1,19 @@
 public class KRS {
-    private String[] matKul = new String[5];
+    private String[] matKul = new String[6];
     private int jmlMatKul = 0;
     private final int isiKRS = 5;
 
     public void tambahMatKul(String matKul) {
-        if(jmlMatKul < isiKRS) {
+        if (jmlMatKul < isiKRS) {
             this.matKul[jmlMatKul] = matKul;
             jmlMatKul++;
         } else {
-            System.out.println(matKul + "Error: KRS telah penuh");
+            // System.out.println(matKul + "Error: KRS telah penuh");
+            this.matKul[jmlMatKul] = matKul + "Error: KRS telah penuh";
         }
     }
+
     public String[] getmatKul() {
-return matKul;
+        return matKul;
     }
 }
